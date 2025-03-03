099863d280971c8ad6778c8ef6aca65f14cec2fd
Alex-Yordan <x.yordan@gmail.com>
2025-03-03 23:13:22 +0300

    ok

--- main.py
+++ main.py
@@ -1,9 +1,9 @@
 import random
 import pygame
 
-pygame.init() ### Инициализация модуля Pygame
+pygame.init()  ### Инициализация модуля Pygame
 
-SCREEN_WIDTH = 800   ### Задаем размеры экрана
+SCREEN_WIDTH = 800  ### Задаем размеры экрана
 SCREEN_HEIGHT = 600
 screen = pygame.display.set_mode((SCREEN_WIDTH, SCREEN_HEIGHT))
 
@@ -16,9 +16,9 @@ target_width = 80
 target_height = 80
 target_x = random.randint(0, SCREEN_WIDTH - target_width)  ### Задаем рандомные координаты появления мишени.
 target_y = random.randint(0, SCREEN_HEIGHT - target_height)
- 
-    ### Задаем рандомный цвет экрана
-color = (random.randint(0,255), random.randint(0,255), random.randint(0,255))
+
+### Задаем рандомный цвет экрана
+color = (random.randint(0, 255), random.randint(0, 255), random.randint(0, 255))
 
 running = True  ### Переменная для создания игрового цикла
 while running:
@@ -26,7 +26,7 @@ while running:
     for event in pygame.event.get():
         if event.type == pygame.QUIT:
             running = False
-        if event.type == pygame.MOUSEBUTTONDOWN: ### Определяет клик мыши по мишени
+        if event.type == pygame.MOUSEBUTTONDOWN:  ### Определяет клик мыши по мишени
             mouse_x, mouse_y = pygame.mouse.get_pos()
             if target_x < mouse_x < target_x + target_width and target_y < mouse_y < target_y + target_height:
                 target_x = random.randint(0, SCREEN_WIDTH - target_width)
@@ -35,9 +35,4 @@ while running:
     screen.blit(target_img, (target_x, target_y))
     pygame.display.update()  ### Обновление экрана
 
-pygame.quit() ### Закрытие модуля PyGame
-
-
-
-
-
+pygame.quit()  ### Закрытие модуля PyGame
