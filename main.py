803f81df8ad30503fea881d48ef5e2259b43bb8a
Alex-Yordan <x.yordan@gmail.com>
2025-03-03 18:24:53 +0300

    add base code

---
+++ main.py
@@ -0,0 +1,21 @@
+import pygame
+
+pygame.init() ### Инициализация модуля Pygame
+
+running = True  ### Переменная для создания игрового цикла
+while running:
+    pass
+
+
+
+
+
+
+
+
+pygame.quit() ### Закрытие модуля PyGame
+
+
+
+
+
